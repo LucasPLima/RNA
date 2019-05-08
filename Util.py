7086d3d93d8c4ca12c5623ee85ca398396a22bfc
Lucas Pereira <lucas.plima.ti@gmail.com>
2019-05-08 00:58:55 -0300

    Adição do critério de escolha das realizações
    Signed-off-by: Lucas Pereira <lucas.plima.ti@gmail.com>

--- Util.py
+++ Util.py
@@ -1,9 +1,11 @@
 from sklearn import preprocessing, datasets
+from sklearn.metrics import confusion_matrix
 import numpy as np
 import matplotlib.pyplot as plt
 import SimplePerceptron.NeuronioPerceptron as ps
 import copy
 import yaml
+import itertools
 
 
 def prepare_data(base):
@@ -15,7 +17,7 @@ def prepare_data(base):
     :return: base de dados normalizada e com uma coluna adicional
     """
     base = base.transpose()
-    normalized = [preprocessing.normalize([base[i]]) for i in range(base.shape[0] - 1)]
+    normalized = [preprocessing.normalize([base[i]], norm='max') for i in range(base.shape[0] - 1)]
     normalized = np.array(normalized)
 
     new_base = np.reshape(normalized, (normalized.shape[0], normalized.shape[2]))
@@ -131,7 +133,6 @@ def plot_decision_region(realization, configurations, iris_cfg, class_names):
     xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                          np.arange(y_min, y_max, plot_step))
 
-    # plt.tight_layout()
     data = np.c_[xx.ravel(), yy.ravel()]
     bias_col = -np.ones(data.shape[0])
 
@@ -157,4 +158,109 @@ def plot_decision_region(realization, configurations, iris_cfg, class_names):
         idx = np.where(y == i)
         plt.scatter(base[idx, 1], base[idx, 2], c=color, label=class_names[i],
                     cmap=plt.cm.RdYlBu, edgecolor='black', s=15)
+
     plt.show()
+
+
+def choose_realization(realizations, criterion_choiced):
+    """
+    Escolhe a melhor realização comparando com a acurácia
+    ou selecionando a taxa mais alta, com base na configuração do cenário
+
+    :param realizations: array contendo as seguintes informações sobre cada realização:
+        (taxa de acerto, array com predições, base de treino, base de teste)
+    :param criterion_choiced: opção de escolha da melhor realização
+                            1 - para a taxa de acerto mais alta
+                            2 - para a taxa de acerto mais próxima da acurácia
+    :return: a melhor realização com base na taxa de acerto média;
+    """
+    accuracy = 0
+    hit_rates = []
+    for realization in realizations:
+        h = realization['hit_rate']
+        accuracy += h
+        hit_rates.append(h)
+
+    accuracy = accuracy / len(realizations)
+    standard_deviation = float(np.std(hit_rates))
+    print('Accuracy: {}%'.format(round(accuracy, 2)))
+    print('Standard Deviation: {}'.format(round(standard_deviation, 2)))
+
+    if criterion_choiced == 1:
+        best_result = max(hit_rates)
+        n = hit_rates.index(best_result)
+        print('Melhor realização: {}'.format(n+1))
+    else:
+        d_means = [accuracy - h for h in hit_rates]
+        nearest_accuracy = min(d_means)
+        n = d_means.index(nearest_accuracy)
+        print('Realização mais próxima da acurácia: {}.'.format(n + 1))
+
+    return realizations[n]
+
+
+def plot_results(realization, configurations):
+    '''
+        Cria visualizações para a base escolhida. As duas visualizações criadas
+        são a Matriz de confusão e as regiões de decisão
+    :param realization: Dicionário contendo informações sobre a melhor realização escolhida
+    :param configurations: Configurações do cenário de testes
+    :return:
+    '''
+    plt.rcParams['figure.figsize'] = (11, 7)
+
+    stream = open('configurations/irisConfigurations.yml', 'r', encoding='utf-8').read()
+    iris_cfg = yaml.load(stream=stream, Loader=yaml.FullLoader)
+
+    y_test = realization['test_base'][:, -1]
+    y_pred = np.array(realization['predict'])
+
+    cnf_matrix = confusion_matrix(y_test, y_pred)
+    np.set_printoptions(precision=2)
+
+    if configurations['chosen_base'] == 'Iris':
+        class_names = ['Iris-{}'.format(iris_cfg['flower_to_classify']), 'Others']
+    else:
+        class_names = ['Class 0', 'Class 1']
+
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names)
+    plt.show()
+
+    if len(realization['weights']) == 3:
+        plot_decision_region(realization, configurations, iris_cfg, class_names)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Matriz de Confusão',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Matriz de confusão normalizada")
+    else:
+        print('Matriz de confusão.')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
