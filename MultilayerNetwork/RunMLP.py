68b34f70a454055ebfa7e1c8859d2b7763586db3
Lucas Pereira <lucas.plima.ti@gmail.com>
2019-08-15 21:15:00 -0300

    Inclusão de informações de log.
    Signed-off-by: Lucas Pereira <lucas.plima.ti@gmail.com>

--- MultilayerNetwork/RunMLP.py
+++ MultilayerNetwork/RunMLP.py
@@ -1,38 +1,62 @@
 from MultilayerNetwork.GenericMultilayerNetwork import MLP
-from Utils import datasets, dataset_utils, model_utils
+from Utils import datasets, dataset_utils, model_utils, log_utils
 import time
+import yaml
 from sklearn.model_selection import train_test_split
+from copy import deepcopy
 import numpy as np
 
 
 def main():
-    dataset, n_features = datasets.load_multiclass_base('Iris')
+    stream = open('configurations/runConfigurations.yml', 'r', encoding='utf-8').read()
+    settings = yaml.load(stream=stream, Loader=yaml.FullLoader)
+
+    dataset, n_features = datasets.load_multiclass_base(settings['chosen_base'])
     n_labels = len(list(set(dataset[:, -1])))
-    epochs = 200
-    initial_learning_rate = 0.5
-    final_learning_rate = 0.1
-    realizations = 20
+    n_output_neurons = 1 if n_labels == 2 else n_labels
+    epochs = settings['epochs']
+    initial_learning_rate = settings['initial_learning_rate']
+    final_learning_rate = settings['final_learning_rate']
+    realizations = settings['realizations']
+
+    total_execution_init = time.time()
+    n_neurons = model_utils.cross_validation(5, [5, 7, 9, 11, 13], deepcopy(dataset), n_features, n_labels, settings['chosen_base'])
+    execution_log = open('execution_logs/{}_{}_neurons.txt'.format(settings['chosen_base'], n_neurons), 'w')
+    scenarios = []
     hit_rates = []
+    execution_log.write('Base escolhida: {}\n'.format(settings['chosen_base']))
+    print('Número de neurônios escohidos: {}.'.format(n_neurons))
     for i in range(realizations):
-        training_base, test_base = train_test_split(dataset, test_size=0.2, stratify=dataset[:, -1])
-        training_base = dataset_utils.binarize_labels(training_base)
-        test_base = dataset_utils.binarize_labels(test_base)
+        training_base, test_base = train_test_split(dataset, test_size=settings['test_size'], stratify=dataset[:, -1])
+        if n_labels > 2:
+            training_base = dataset_utils.binarize_labels(training_base)
+            test_base = dataset_utils.binarize_labels(test_base)
         start = time.time()
-        mlp_test = MLP(n_layers=2, n_neurons=[6, n_labels], n_features=n_features)
+        mlp_test = MLP(n_layers=2, n_neurons=[n_neurons, n_output_neurons], n_features=n_features)
         mlp_test.training(training_base, epochs, initial_learning_rate=initial_learning_rate,
                           final_learning_rate=final_learning_rate)
         end = time.time()
         total_time = (end - start)
         predicted_labels, activations = mlp_test.predict(test_base)
         hit_rate = model_utils.hit_rate(predicted_labels, test_base[:, n_features+1:], activations)
+        scenarios.append({'hit_rate': hit_rate,
+                          'ol_net': deepcopy(mlp_test),
+                          'training_base': training_base,
+                          'test_base': test_base})
         hit_rates.append(hit_rate)
-        print('Realizacação: {} | Tempo de treinamento:{} | Hit rate: {}%'.format(i + 1, round(total_time, 2), hit_rate))
-        print('---------------------------------------------------------')
+        print('Realização: {} | Tempo de treinamento:{} | Taxa de acerto: {}%'.format(i + 1, round(total_time, 2), hit_rate))
+        print('----------------------------------------------------------------------')
+        execution_log.write('Realização: {} | Tempo de treinamento:{} | Taxa de acerto: {}%\n'.format(i + 1, round(total_time, 2), hit_rate))
+        execution_log.write('----------------------------------------------------------------------\n')
 
-    accuracy = float(np.mean(hit_rates))
-    print('Acurácia: {}%'.format(round(accuracy, 2)))
-    # TODO
-    # printar região de decisão
+    execution_log.write('Acurácia: {}%\nDesvio Padrão:{}\n'.format(round(float(np.mean(hit_rates)), 2),
+                                                                 round(float(np.std(hit_rates)),2)))
+    best_realization = log_utils.choose_realization(scenarios, settings['criterion_choiced'])
+    total_execution_final = time.time()
+
+    print('Tempo de execução total:{} segundos'.format(round(total_execution_final-total_execution_init)))
+    execution_log.write('Tempo de execução total:{} segundos'.format(round(total_execution_final-total_execution_init)))
+    execution_log.close()
 
 
 if __name__ == '__main__':
--- Utils/model_utils.py
+++ Utils/model_utils.py
@@ -1,20 +1,28 @@
 import numpy as np
+from MultilayerNetwork import GenericMultilayerNetwork as gmn
+from sklearn.model_selection import KFold
+from Utils import dataset_utils
+import yaml
 
 
 def validate_predict(predict, activations):
     max_activation = max(activations)
-    if sum(predict) == 0:
-        predict[activations.index(max_activation)] = 1
-    elif sum(predict) > 0:
-        predict = [0 for i in range(len(predict))]
-        predict[activations.index(max_activation)] = 1
+    new_predict = predict
+    if len(predict) > 1:
+        if sum(predict) == 0:
+            new_predict[activations.index(max_activation)] = 1
+        elif sum(predict) > 1:
+            new_predict = [0 for i in range(len(predict))]
+            new_predict[activations.index(max_activation)] = 1
+
+    return new_predict
 
 
 def hit_rate(predicted_labels, desired_labels, activations=None):
     hit = []
     for i in range(len(predicted_labels)):
-        validate_predict(predicted_labels[i], activations[i])
-        error = desired_labels[i] - predicted_labels[i]
+        new_predict = validate_predict(predicted_labels[i], activations[i])
+        error = desired_labels[i] - new_predict
         if sum(error**2) == 0:
             hit.append(1)
 
@@ -25,3 +33,46 @@ def hit_rate(predicted_labels, desired_labels, activations=None):
 def eta_decay(actual_epoch, final_epoch, initial_learning_rate, final_learning_rate):
     new_eta = initial_learning_rate * ((final_learning_rate / initial_learning_rate)**(actual_epoch/final_epoch))
     return new_eta
+
+
+def cross_validation(splits, n_hidden_neurons, base, n_features, n_classes, chosen_base):
+    print('Cross Validation init:')
+
+    stream = open('configurations/runConfigurations.yml', 'r', encoding='utf-8').read()
+    settings = yaml.load(stream=stream, Loader=yaml.FullLoader)
+    log_file = open('grid_search_log/{}_log.txt'.format(chosen_base), 'w')
+
+    folds = KFold(n_splits=splits)
+    np.random.shuffle(base)
+    grid_search_base = base[0:int(round((1-settings['test_size'])*base.shape[0])), :]
+    n_output_neurons = 1 if n_classes == 2 else n_classes
+
+    if n_classes > 2:
+        grid_search_base = dataset_utils.binarize_labels(grid_search_base)
+    accuracy = []
+    for n in range(len(n_hidden_neurons)):
+        realization = 0
+        hit_rates = []
+        print('Number of hidden neurons:{}'.format(n_hidden_neurons[n]))
+        log_file.write('Number of hidden neurons:{}\n'.format(n_hidden_neurons[n]))
+        for train_index, test_index in folds.split(grid_search_base):
+            training_base = grid_search_base[train_index, :]
+            test_base = grid_search_base[test_index, :]
+            mlp = gmn.MLP(2, [n_hidden_neurons[n], n_output_neurons], n_features)
+            mlp.training(training_base, epochs=settings['epochs'],initial_learning_rate=settings['initial_learning_rate'],
+                         final_learning_rate=settings['final_learning_rate'])
+            predicted_labels, activations = mlp.predict(test_base)
+            hit = hit_rate(predicted_labels, test_base[:, n_features + 1:], activations)
+            print('\tHit rate for realization {}: {}%'.format(realization + 1, hit))
+            log_file.write('\tHit rate for realization {}: {}%\n'.format(realization + 1, hit))
+            hit_rates.append(hit)
+            realization += 1
+            del mlp
+        accuracy.append(np.mean(hit_rates))
+        print('\tAccuracy with {} hidden neurons: {}%'.format(n_hidden_neurons[n], round(accuracy[n], 2)))
+        log_file.write('\tAccuracy with {} hidden neurons: {}%\n'.format(n_hidden_neurons[n], round(accuracy[n], 2)))
+
+    print('Cross Validation end.')
+    log_file.close()
+    best_realization = max(accuracy)
+    return n_hidden_neurons[accuracy.index(best_realization)]
